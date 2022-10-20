352cbf59c03cd7aca253f0fc8041294db9fd4d1e
Paul Realpe <paul.realpe@ia-aplicada.com>
2022-10-19 22:11:39 -0500

    Feat: Upload file

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:html';
+import 'dart:typed_data';
+
 import 'package:barcode_generator/shared/custom_button.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -17,7 +21,16 @@ class HomePage extends StatelessWidget {
             children: [
               CustomButton(
                 text: 'Descargar Plantilla',
-                onPressed: () => print('Hola Amiguis'),
+                onPressed: () {
+                  const String url =
+                      'https://docs.google.com/spreadsheets/d/1sd8sU_i2aoj2vFHADu6j0_kyNWGD0zt_/edit?usp=sharing&ouid=113850207277339374301&rtpof=true&sd=true';
+                  AnchorElement anchorElement = AnchorElement(href: url);
+                  anchorElement.download = 'url';
+                  document.body?.append(anchorElement);
+                  anchorElement.click();
+
+                  anchorElement.remove();
+                },
                 icon: Icons.cloud_download,
                 width: 220,
                 backgroundColor: Colors.transparent,
@@ -27,7 +40,26 @@ class HomePage extends StatelessWidget {
               ),
               CustomButton(
                 text: 'Cargar Plantilla',
-                onPressed: () => print('Hola Amiguis'),
+                onPressed: () async {
+                  FilePickerResult? result;
+                  try {
+                    result = await FilePicker.platform.pickFiles(
+                      type: FileType.custom,
+                      allowedExtensions: ['xls', 'xlsx'],
+                    );
+                  } catch (e) {
+                    print(e);
+                  }
+
+                  if (result != null) {
+                    try {
+                      Uint8List uploadfile = result.files.single.bytes!;
+                      print(uploadfile);
+                    } catch (e) {
+                      print(e);
+                    }
+                  }
+                },
                 icon: Icons.cloud_upload_rounded,
                 width: 220,
               ),
